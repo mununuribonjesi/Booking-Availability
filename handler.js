299624c7c3f2c1e6aa35b79952bbb25ce4d2f876
mbonjesi@gmail.com <mununuribonjesi@Mununuris-MacBook-Pro.local>
2021-03-15 02:12:58 +0000

    serverless

--- handler.js
+++
@@ -1,8 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
-
-const server = awsServerlessExpress.createServer(app);
-
-module.handler = (event,context) => {
-    return awsServerlessExpress.proxy(server,event,context)
-}
